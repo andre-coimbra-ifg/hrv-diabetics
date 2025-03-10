03e3a9581321d5cf87e5568774c861670c3823c6
Andre Coimbra <andre.coimbra@ifg.edu.br>
2025-03-09 22:46:52 -0300

    modified to return sorted list

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,12 @@ import logging
 
 def list_rr_files(directory):
     """Lista todos os arquivos RR em um diretório."""
-    files = [
-        os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(".txt")
-    ]
+    files = sorted(
+        [
+            os.path.join(directory, f)
+            for f in os.listdir(directory)
+            if f.endswith(".txt")
+        ]
+    )
     logging.info(f"{len(files)} arquivo(s) encontrado(s) em {directory}")
     return files
